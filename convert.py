97d9a125ebeab752efc7624883943a626b3be579
Meow Lu <ttsmcpe@gmail.com>
2023-02-17 12:22:26 +0800

    增加註解

--- convert.py
+++ convert.py
@@ -16,63 +16,61 @@ with open('data.json') as f:
 
 # 確認首文字對應英文
 def checkWord(word):
-  if word not in convertDict:
-    inputWord = input( word + " | INPUT WORD: ") # 如果沒有資料可以輸入定義字首（長度不可大於 1 ）
-    while(len(inputWord) > 1): # 太長需要重新輸入
-        inputWord = input( word + " | INPUT WORD: ") 
-    convertDict[word] = inputWord
-  return convertDict[word] # 回傳文字
+    if word not in convertDict:
+        inputWord = input( word + " | INPUT WORD: ") # 如果沒有資料可以輸入定義字首（長度不可大於 1 ）
+        while(len(inputWord) > 1): # 太長需要重新輸入
+            inputWord = input( word + " | INPUT WORD: ") 
+        convertDict[word] = inputWord
+    return convertDict[word] # 回傳文字
 
-# 檔案清單取得(目前支援：FLAC、MP3)
-fileList = []
+# 檔案清單取得(目前支援：FLAC)
+filePathList = []
 for targetPattern in [r"*.flac"]: 
     #,r"*.mp3"
     for path in glob.glob(targetPattern):
-        fileList.append(path)
+        filePathList.append(path)
         print(path,"add in list")
 
-# 修改ID3 TAG
-for fileName in fileList:
-    # 原先資料
-    tags = mutagen.File(fileName)  
-    tag_artist = tags["artist"][0]
-    tag_album = tags["album"][0]
-    tag_title = tags["title"][0]
-    print(tag_artist,tag_album,tag_title)
-    
-    tagToChange = ["artist","album","title"]
-    newTagData = {}
-    
-    for tagItem in tagToChange:
-        newTagData[tagItem+'sort'] =  checkWord(tags[tagItem][0][0]) + cc.convert(tags[tagItem][0])
-        newTagData[tagItem] = cc.convert(tags[tagItem][0])
-    print(newTagData)
+# 修改ID3 TAG（增加 SORT 標籤，簡體繁體轉換）
+errorFileList = []
+for filePath in filePathList:
+    try:
+        # 原先資料
+        tags = mutagen.File(filePath)  
+        tag_artist = tags["artist"][0]
+        tag_album = tags["album"][0]
+        tag_title = tags["title"][0]
+        print(tag_artist,tag_album,tag_title)
+        
+        # 標籤資料定義
+        tagToChange = ["artist","album","title"] #作用的原始標籤
+        newTagData = {} #定義新寫入的標籤清單
+        for tagItem in tagToChange:
+            newTagData[tagItem+'sort'] =  checkWord(tags[tagItem][0][0]) + cc.convert(tags[tagItem][0]) #SORT 標籤
+            newTagData[tagItem] = cc.convert(tags[tagItem][0]) # 簡繁轉換新名稱
+        print(newTagData)
 
-    # SORT 標籤資料定義
-    """
-    artistsort = checkWord(tag_artist[0]) + tag_artist
-    albumsort = checkWord(tag_album[0]) + tag_album
-    titlesort = checkWord(tag_title[0]) + tag_title
-    print(artistsort,albumsort,titlesort)
-    """
-    # 檔案型態套用（副檔名判斷）
-    if(".flac" in fileName):
-        audio = FLAC(fileName)
-    #if(".mp3" in fileName):
-        #audio = MP3(fileName)
-    # 修改標籤
-    for tag in newTagData.items():
-        audio[tag[0]] = tag[1]
-    """
-    audio["artistsort"] = artistsort
-    audio["albumsort"] = albumsort
-    audio["titlesort"] = titlesort
-    """
-    audio.pprint()
-    audio.save() 
-    tags = mutagen.File(fileName)
-    print("--------------------------------")
+        # 檔案型態套用（副檔名判斷）
+        if(".flac" in filePath):
+            audio = FLAC(filePath)
+
+        for tag in newTagData.items():
+            audio[tag[0]] = tag[1] # 寫入新標籤
+        
+        audio.pprint()
+        audio.save() #檔案寫入
+        #-tags = mutagen.File(filePath)
+        print("--------------------------------")
+
+    except Exception as e:
+        errorFileList.append({filePath:e}) # 錯誤紀錄報告
+
 print("所有資料轉換完成")
+if len(errorFileList) > 0: #如果有錯誤會輸出
+    print("錯誤清單")
+    for item in errorFileList:
+        print(item)
+    print("--------------------------------")
 
 # 回存文字轉換資料
 with open('data.json', 'w') as f:
--- trans.py
+++ trans.py
@@ -3,36 +3,42 @@ from opencc import OpenCC
 cc = OpenCC('s2t')
 
 fileList = []
-fileList = glob.glob(r"*lrc")
-failList = []
+fileList = glob.glob(r"*lrc") # 取得ＬＲＣ檔案
+errFileList = []
+
+# 編碼設定
 setcodeType = ""
-#setcodeType = input("Input encoding ( GB2312[g] / utf-8 [u] ): ")
+setcodeType = input("Input encoding ( QQ音樂預設-GB2312[g] / utf-8 [u] ): ")
 if(setcodeType=="g" or setcodeType=="G"):
     setcodeType = "GB2312"
 elif(setcodeType=="u" or setcodeType=="U" or setcodeType ==""):
     setcodeType = "UTF-8"
 
+# 轉換
 for docPath in fileList:
     try:
         print('Encode Converting...',docPath)
         readContent = ""
         with open(docPath,"r",encoding = setcodeType) as inFile:
-            readContent = inFile.read()
+            readContent = inFile.read()  # 讀取檔案
         with open(docPath,"w", encoding="UTF-8") as outFile:
-            outFile.write(readContent)
+            outFile.write(readContent)  # 寫入文件
+        
     except Exception as e:
         print("Error")
         print(e)
-        failList.append(docPath)
+        errFileList.append(docPath)
     
     try:
         print('Translating...',docPath)
         newLrc = ''
         f = open(docPath, 'r', encoding="UTF-8")
         for line in f.readlines():
-            newLrc = newLrc + cc.convert(line)
+            newLrc = newLrc + cc.convert(line)  #讀取歌詞 
         f.close()
-        #print(newLrc)
+        print(newLrc[0:20])
+
+        # 儲存檔案
         savePath = cc.convert(docPath)[0:cc.convert(docPath).find(" - ")] + "/" + cc.convert(docPath)
         f = open(savePath, 'w+', encoding="UTF-8")
         f.write(newLrc)
@@ -40,11 +46,13 @@ for docPath in fileList:
     except Exception as e:
         print("Error")
         print(e)
-        failList.append(docPath)
+        errFileList.append(docPath)
 
-    print('-------------------------')
+    print('--------------------------------')
 
-print('ALL DONE')
-if len(failList) > 0:
+print('翻譯檔案完成 ',len(fileList)-len(errFileList))
+
+# 錯誤檔案輸出
+if len(errFileList) > 0:
     print('Fail List:')
-    print(failList)
+    print(errFileList)
